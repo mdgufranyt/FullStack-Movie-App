78ea6b3b6932291fd52f4b15ba912af1a0bca685
mdgufranyt <gufranbareilly@gmail.com>
2026-02-21 13:14:45 +0530

    Fix Bugs & Clean Structure

--- src/helper.js
+++
@@ -1,7 +0,0 @@
-// Automatically detect environment and set API base URL
-export const api_base_url =
-  process.env.NODE_ENV === "development" ||
-  window.location.hostname === "localhost" ||
-  window.location.hostname === "127.0.0.1"
-    ? "http://localhost:3000" // Local development
-    : "/api"; // Production (Vercel)
